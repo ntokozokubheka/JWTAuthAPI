d5fb84eb40fb8c396e7ef12545256695e0201d63
Stokza <37531253+ntokozokubheka@users.noreply.github.com>
2023-05-18 13:57:55 +0200

    Edits on Repo

---
+++ Models/UserInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace JWTAUTH.API.Models
+{
+    public class UserInfo
+    {
+        public int UserId { get; set; }
+        public string? DisplayName { get; set; }
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public DateTime? CreatedDate { get; set; }
+
+    }
+}
